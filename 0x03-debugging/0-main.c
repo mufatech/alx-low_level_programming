73075a4ab44988b4a2078efaedcdda4a159cd59d
mufatech <fabosedesidiqat@gmail.com>
2022-12-13 03:45:59 -0800

    task done

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* written by mufatech
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
