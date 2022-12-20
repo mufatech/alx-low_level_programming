cfc74944213ffcc364040eb41110b0956a7cf68c
mufatech <fabosedesidiqat@gmail.com>
2022-12-20 03:33:14 -0800

    task done

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
-*  reset_to_98 - takes the pointer and updates the value
-*  @n: int to check
-*  written by mufatech
-*
-*  Return: 0
-*/
-
-void void reset_to_98(int *n)
+ * reset_to_98 - takes the pointer int and updates the value
+ * @n: int to check
+ * written by mufatech
+ *
+ * Return: 0
+ */
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
