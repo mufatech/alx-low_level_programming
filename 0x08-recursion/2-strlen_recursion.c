b32f47e02460d37e1d48627a5b15d61e81169272
mufatech <fabosedesidiqat@gmail.com>
2023-01-04 04:48:53 -0800

    task 2 done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strlen_recursion - length of string
+ * @s:string
+ *
+ * written by mufatech
+ *
+ * Return:int
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s)
+	{
+		i++;
+		i += _strlen_recursion(s + 1);
+
+	}
+	return (i);
+}
