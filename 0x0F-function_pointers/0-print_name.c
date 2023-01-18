7c23fbcea9f71f0ccc7e22e4e19f0ae6cf45c93c
mufatech <fabosedesidiqat@gmail.com>
2023-01-18 04:19:10 -0800

    task 1 done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,22 +1,18 @@
-#include <stdio.h>
 #include "function_pointers.h"
+
 /**
- * print_name_as_is - prints a name as is
+ * print_name - prints a name
  * @name: name of the person
  * @f: function of name
  *
  * written by mufatech
  *
- * Return: Nothing.
+ * Return: void
  */
 
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, void (*f)(char *))
 {
+	
 	if (name != NULL && f != NULL)
-
-	f(name);
-
-	{
-	printf("My name is %s\n", name);
-	}
+		f(name);
 }
