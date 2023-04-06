ed0289288302e37f3fa34570785fed3f23295d5f
mufatech <fabosedesidiqat@gmail.com>
2023-04-06 06:08:42 -0700

    Hash Tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * compiled: mufatech
+ *
+ * Return: pointer to the hash table newly created
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table;
+	unsigned long int j;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+	for (j = 0; j < size; j++)
+		hash_table->array[j] = NULL;
+	return (hash_table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,2 @@
+0x1A. C - Hash tables
+This file consists of tasks on Hash Tables on C-Programming Algorithm Data structure of ALX SOftware Engineering Program.
