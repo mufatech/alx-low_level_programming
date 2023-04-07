9e8fb8cd66e72240f2683f7af3edfed89e0c69dd
mufatech <fabosedesidiqat@gmail.com>
2023-04-06 22:13:09 -0700

    Hash Tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,14 +2,48 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
+
+/**
+ * make_hash_node - creates a new hash node
+ * @key: key for the node
+ * @value: value for the node
+ *
+ * Return: the new node, or NULL if failed
+ */
+
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+
 /**
  * hash_table_set - sets a key to a value in the hash table
  * @ht: hash table to add or update the key to
  * @key: key for the data to be add or update
  * @value: data to store
  *
- * Return: 1 if succeeded, 0 otherwise
+ * Return: 1 if succeeded, 0 if failed
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
